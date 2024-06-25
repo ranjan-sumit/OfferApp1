f4f359e383d0129dcf3841ce6e55f3784da9e8c8
Sumit Ranjan <36401733+ranjan-sumit@users.noreply.github.com>
2024-06-25 14:38:32 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,40 +1,40 @@
-import psycopg2 as pg
-import sqlalchemy
-from sqlalchemy import create_engine
-import pandas as pd
-import os
-#import nzpy
+# import psycopg2 as pg
+# import sqlalchemy
+# from sqlalchemy import create_engine
+# import pandas as pd
+# import os
+# import nzpy
 
-def net_conn():
+# def net_conn():
     
-    return netezza_conn
+#     return netezza_conn
 
-def init_connection():
+# def init_connection():
     
 
     
-    postgres_conn = create_engine(postgre_uri)
-    return postgres_conn
+#     postgres_conn = create_engine(postgre_uri)
+#     return postgres_conn
 
-postgres_conn = init_connection()
-
-
+# postgres_conn = init_connection()
 
 
-def get_offer_shareid(shareId):
+
+
+# def get_offer_shareid(shareId):
     
-    get_offer = pd.read_sql(f""" SELECT OFFER_4 FROM ADMIN.BUDDY_PROJECT WHERE card_key like '%{shareId}'; """, net_conn())
-    return get_offer
+#     get_offer = pd.read_sql(f""" SELECT OFFER_4 FROM ADMIN.BUDDY_PROJECT WHERE card_key like '%{shareId}'; """, net_conn())
+#     return get_offer
 
-def get_offer_emailid(email_id):
+# def get_offer_emailid(email_id):
     
-    get_offer = pd.read_sql(f""" SELECT * FROM ADMIN.BUDDY_PROJECT
-WHERE CUST_EMAIL='{email_id}'
-AND EMAIL_FLAG='VALID_FORMAT'; """, net_conn())
-    return get_offer
+#     get_offer = pd.read_sql(f""" SELECT * FROM ADMIN.BUDDY_PROJECT
+# WHERE CUST_EMAIL='{email_id}'
+# AND EMAIL_FLAG='VALID_FORMAT'; """, net_conn())
+#     return get_offer
 
-def get_offer_phoneno(phoneno):
+# def get_offer_phoneno(phoneno):
     
-    get_offer = pd.read_sql(f""" SELECT * FROM ADMIN.BUDDY_PROJECT
-WHERE CUST_MOBILE like '%{phoneno}' ; """, net_conn())
-    return get_offer
+#     get_offer = pd.read_sql(f""" SELECT * FROM ADMIN.BUDDY_PROJECT
+# WHERE CUST_MOBILE like '%{phoneno}' ; """, net_conn())
+#     return get_offer
